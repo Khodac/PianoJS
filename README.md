bcfea03359856ec4c1d515aaf7c717c527642752
Pablo Caballero <pablo.caballero.marin@outlook.es>
2023-07-28 19:51:29 +0200

    cambios de sonido

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # PianoJS
+Este es un piano interactivo desarrollado utilizando JavaScript, HTML y CSS. Permite reproducir sonidos de piano tanto haciendo clic en las teclas como utilizando el teclado.
+
+## Requisitos
+
+*Navegador web moderno que admita HTML5, CSS3 y JavaScript.
+
+## Estructura del proyecto
+
+El proyecto constará de tres archivos:
+
+1. index.html: el archivo HTML que contiene la estructura del piano.
+2. style.css: el archivo CSS que se encarga del estilo del piano.
+3. script.js: el archivo JavaScript que maneja la interacción con las teclas y reproduce los sonidos.
+
+## Como utilizar el piano interactivo
+
+1. Clona este repositorio con el comando "git clone" y entra en la carpeta.
+2. Abre el archivo index.html en tu navegador web.
+3. Verás un piano interactivo en la página. Puedes hacer clic en las teclas del piano o utilizar el teclado para reproducir los sonidos.
+
+¡Disfruta del piano interactivo!
---
+++ piano.js
@@ -0,0 +1,76 @@
+
+
+const piano_Teclas = document.querySelectorAll(".pianoTeclas .tecla");
+
+let audio = new Audio("sounds/a.mp3");
+
+const sonidoTecla = (tecla) => {
+    audio.src = `sounds/${tecla}.mp3`;
+    audio.play();
+    
+    const teclaPulsada = document.querySelector(`[data-tecla=${tecla}]`);
+    teclaPulsada.classList.add("hover"); 
+    setTimeout(()=> { teclaPulsada.classList.remove("hover");
+    }, 150);
+    }
+//
+piano_Teclas.forEach(tecla =>{
+    tecla.addEventListener("click", () => sonidoTecla(tecla.dataset.tecla));
+})
+
+const teclaPulsada = (e) => {
+    sonidoTecla(e.key);
+}
+
+document.addEventListener("keydown", teclaPulsada);
+
+
+
+
+
+function isDevicePortrait() {
+    return window.innerWidth <= 768 && window.innerHeight >= window.innerWidth;
+}
+
+
+function toggleRotateMessage(show) {
+    const messageContainer = document.querySelector('.message-container');
+    if (show) {
+        messageContainer.style.display = 'flex'; 
+    } else {
+        messageContainer.style.display = 'none'; 
+    }
+}
+
+
+function applyPianoRotation() {
+    const pianoDiv = document.querySelector("div");
+
+    if (isDevicePortrait()) { 
+        pianoDiv.style.display = 'none'; 
+        toggleRotateMessage(true); 
+    } else {
+        pianoDiv.style.display = 'flex'; 
+        pianoDiv.style.transform = 'none';
+        pianoDiv.style.transformOrigin = 'unset';
+        pianoDiv.style.height = '420px'; 
+        pianoDiv.style.width = '80%';
+        pianoDiv.style.position = 'relative';
+        pianoDiv.style.top = 'unset';
+        pianoDiv.style.left = 'unset';
+        toggleRotateMessage(false); // Ocultar el mensaje
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    applyPianoRotation();
+
+    window.addEventListener("resize", function () {
+        applyPianoRotation();
+    });
+
+    window.addEventListener("orientationchange", function () {
+        applyPianoRotation();
+    });
+});
